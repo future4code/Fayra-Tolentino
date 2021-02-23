73783bd10a48fd4ffc99ba6aceace74ea90a7397
Fayra Miranda <miranda.fayra@gmail.com>
2021-02-23 15:43:26 -0300

    Finalizar exercicios js aula 2

---
+++ semana3.1/aula/exp_aula/script.js
@@ -0,0 +1,48 @@
+// console.log("Harry Potter");
+// // Soma +
+// const valor1 = 10;
+// const valor2= 20;
+// const soma = valor1+ valor2;
+// console.log("Valor do resultado é" , soma);
+
+// // Subtração -
+// const valor1 = 10;
+// const valor2= 20;
+// const subtracao = valor1-valor2;
+// console.log("Valor do resultado é" , subtracao);
+
+// // Multiplicação *
+// const valor1 = 10;
+// const valor2= 20;
+// const multiplicacao = valor1*valor2;
+// console.log("Valor do resultado é" , multiplicacao);
+
+// // Divisão /
+// const valor1 = 10;
+// const valor2= 20;
+// const divisao = valor1/valor2;
+// console.log("Valor do resultado é" , divisao);
+
+// // Resto %
+// const valor1 = 10;
+// const valor2= 20;
+// const resto = valor1%valor2;
+// console.log("Valor do resultado é" , resto);
+
+// Simplificação => para não repetir o número 
+
+let resultado = 10;
+// resultado = resultado + 20;
+resultado += 20;
+
+
+
+
+
+
+
+
+
+
+
+
