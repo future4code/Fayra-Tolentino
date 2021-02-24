e9cccba1370c675551aa4880aa3125ec114f7ad0
Fayra Miranda <miranda.fayra@gmail.com>
2021-02-24 13:33:06 -0300

    finalizar exercicio 1

---
+++ SEMANA3/condicionais/script.js
@@ -0,0 +1,28 @@
+// EXERCÍCIO 1
+
+// Leia o código abaixo:
+
+
+const respostaDoUsuario = prompt("Digite o número que você quer testar")
+const numero = Number(respostaDoUsuario)
+
+if(numero % 2 === 0) {
+  console.log("Passou no teste.")
+} else {
+  console.log("Não passou no teste.")
+}
+
+// Explique o que o código faz. Qual o teste que ele realiza? 
+// Para que tipos de números ele imprime no console "Passou no teste"? Para que tipos de números
+// a mensagem é "Não passou no teste"?
+
+// Resposta:
+
+// O código a cima ele solicita ao usuário por meio de um prompt, um número que o usário deseja testar. 
+// após essa etapa, o algoritmo transforma em número, a string obtida pelo o input do usuário, verifica se o resto 
+// do número é 0. Se sim, o número  dado pelo usuário passou no TextDecoderStream. Se não, o número do usuário não passou
+// no teste .
+
+// ----------------------------------------------------------------------------------------------------------------------------------
+
+// EXERCICIO 2
