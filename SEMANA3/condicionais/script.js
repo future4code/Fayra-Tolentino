13bcd49469d91f2fdeddf721d6e41624ce83f788
Fayra Miranda <miranda.fayra@gmail.com>
2021-02-24 14:14:38 -0300

    Finalizar exercicio 4

--- SEMANA3/condicionais/script.js
+++ SEMANA3/condicionais/script.js
@@ -3,14 +3,14 @@
 // Leia o código abaixo:
 
 
-const respostaDoUsuario = prompt("Digite o número que você quer testar")
-const numero = Number(respostaDoUsuario)
+// const respostaDoUsuario = prompt("Digite o número que você quer testar")
+// const numero = Number(respostaDoUsuario)
 
-if(numero % 2 === 0) {
-  console.log("Passou no teste.")
-} else {
-  console.log("Não passou no teste.")
-}
+// if(numero % 2 === 0) {
+//   console.log("Passou no teste.")
+// } else {
+//   console.log("Não passou no teste.")
+// }
 
 // Explique o que o código faz. Qual o teste que ele realiza? 
 // Para que tipos de números ele imprime no console "Passou no teste"? Para que tipos de números
@@ -30,26 +30,26 @@ if(numero % 2 === 0) {
 // O código abaixo foi feito por uma pessoa desenvolvedora, contratada para automatizar algumas tarefas de um supermercado. 
 // Veja abaixo:
 
-let fruta = prompt("Escolha uma fruta")
-let preco
-switch (fruta) {
-  case "Laranja":
-    preco = 3.5
-    break;
-  case "Maçã":
-    preco = 2.25
-    break;
-  case "Uva":
-    preco = 0.30
-    break;
-  case "Pêra":
-    preco = 5.5
-    break; // BREAK PARA O ITEM c.
-  default:
-    preco = 5
-    break;
-}
-console.log("O preço da fruta ", fruta, " é ", "R$ ", preco)
+// let fruta = prompt("Escolha uma fruta")
+// let preco
+// switch (fruta) {
+//   case "Laranja":
+//     preco = 3.5
+//     break;
+//   case "Maçã":
+//     preco = 2.25
+//     break;
+//   case "Uva":
+//     preco = 0.30
+//     break;
+//   case "Pêra":
+//     preco = 5.5
+//     break; // BREAK PARA O ITEM c.
+//   default:
+//     preco = 5
+//     break;
+// }
+// console.log("O preço da fruta ", fruta, " é ", "R$ ", preco)
 
 
 // a. Para que serve o código acima?
@@ -71,14 +71,14 @@ console.log("O preço da fruta ", fruta, " é ", "R$ ", preco)
 
 // Leia o código abaixo:
 
-const numero = Number(prompt("Digite o primeiro número."))
+// const numero = Number(prompt("Digite o primeiro número."))
 
-if(numero > 0) {
-    console.log("Esse número passou no teste")
-    let mensagem = "Essa mensagem é secreta!!!"
-}
+// if(numero > 0) {
+//     console.log("Esse número passou no teste")
+//     let mensagem = "Essa mensagem é secreta!!!"
+// }
 
-console.log(mensagem)
+// console.log(mensagem)
 
 
 // a. O que a primeira linha está fazendo?
@@ -94,3 +94,21 @@ console.log(mensagem)
 // pelo que está a fora.
 
 // -------------------------------------------------------------------------------------------------------------------
+// **Exercícios de escrita de código**
+
+// EXERCÍCIO 4
+
+// Vamos criar programas que lidem com entradas do usuário! Faça um programa que pergunta ao usuário qual a idade dele e imprima
+// no console se ele/ela pode dirigir (apenas maiores de idade).
+
+// 1. Faça um `prompt` para receber a idade do usuário e guarde em uma variável.
+let idade = prompt("Insira sua idade");
+// 2. Garanta que essa variável é do tipo `Number`, você deve usar o cast para number para isso.
+let numero = Number(idade);
+// 3. Agora veja se essa idade do usuário corresponde à idade mínima que permite dirigir. Se sim, 
+// imprima no console  `"Você pode dirigir"`, caso contrário, imprima `"Você não pode dirigir."`
+if(numero >=18){
+    console.log("Você pode dirigir");
+}else{
+    console.log("Você é menor de idade, portanto não pode dirigir =(!");
+}
