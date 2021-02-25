fdfecaa61ed06327b929012f268966fd769c7a86
Fayra Miranda <miranda.fayra@gmail.com>
2021-02-25 15:38:27 -0300

    Escrever exercício

--- SEMANA3/casa/operadores/js-vanilla-template/src/index.js
+++ SEMANA3/casa/operadores/js-vanilla-template/src/index.js
@@ -127,3 +127,43 @@
 
 // --------------------------------------------------------------------------------------
 
+// DESAFIOS
+
+// 1.Para este exercício, será necessário o conhecimento das fórmulas para mudar a unidade de temperatura entre
+//  Graus Celsius(°C),  Graus Fahrenheit(°F) e Kelvin(K). Abaixo estão duas delas:
+
+//=> Graus Fahrenheit(°F) para Kelvin(K)
+// (KELVIN) = (GRAUS_FAHRENHEIT - 32)*5/9 + 273.15
+
+//=> Graus Celsius(°C) para Graus Fahrenheit (°C)
+// (GRAUS_FAHRENHEIT) = (GRAUS_CELSIUS)*9/5 + 32
+
+// a. Calcule e mostre o valor de 77°F em  K, mostrando a unidade no console também.
+
+// b. Calcule e mostre o valor de 80°C em °F, mostrando a unidade no console também.
+
+// c. Calcule e mostre o valor de 30°C em °F e K, mostrando as unidades no console também.
+
+// d. Altere o último item para que o usuário insira o valor em graus Celsius que ele deseja converter.
+
+let kelvin;
+let fahrenheit;
+let celsius;
+
+kelvin = (fahrenheit-32)*5/9 +273.15;
+fahrenheit=(celsius)*9/5 + 32;
+
+// a 
+// var graus = prompt("Quantos º F deseja obter em ºK ?");
+// console.log()
+
+
+let numeroPedido = prompt("Qual o grau em ºF que você deseja converter para Kelvin");
+kelvin = (numeroPedido-32)*5/9 +273.15;
+console.log("Convertendo " + numeroPedido + " em ºF para º Kelvin, temos " , kelvin );
+
+let numeroCelsius = prompt("Qual o grau em ºC que você deseja converter para Fahrenheit?");
+fahrenheit=(numeroCelsius)*9/5 + 32;
+console.log("Convertendo " + numeroCelsius + " em ºC para º Fahrenheit, temos " , fahrenheit );
+
+
