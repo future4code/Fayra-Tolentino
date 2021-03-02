329424d31cd314896c4f344e1743588bc7010669
Fayra Miranda <miranda.fayra@gmail.com>
2021-03-02 15:17:53 -0300

    Finalizar exs 3 e 4

--- SEMANA4/planner/planner/index.js
+++ SEMANA4/planner/planner/index.js
@@ -4,11 +4,16 @@
 // 2 - Dentro da função, capture o valor do campo do HTML (que é a tarefa a ser colocada no planner)
 //  e armazene em uma variável
 
+// 3 - Capture o valor do seletor do HTML (o dia da semana selecionado) e armazene em uma variável.
+
 function criarNovaTarefa(){
     const gerarTarefa = document.getElementById("gerar-tarefa");
     // console.log("tenho o Dom!")
-    const minhaTarefa = document.getElementById("tarefa");
-    // console.log("Boaaa Garota!");
+    const minhaTarefa = document.getElementById("tarefa").value;;
+    console.log(minhaTarefa);
+    const diasSemana = document.getElementById("dias-semana").value;
+    console.log(diasSemana)
+    
 
 }
 
