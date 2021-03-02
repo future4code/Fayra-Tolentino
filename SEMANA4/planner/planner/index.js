f253ae239ec6d3c54ada4a893246c7c5eb2cd49a
Fayra Miranda <miranda.fayra@gmail.com>
2021-03-02 14:37:00 -0300

    Finalizar exs 1

---
+++ SEMANA4/planner/planner/index.js
@@ -0,0 +1,7 @@
+// 1 - Crie uma função que é chamada ao clicar no botão "Criar Tarefa". 
+// Use um evento apropriado que identifique o clique no botão
+
+function criarNovaTarefa(){
+    const gerarTarefa = document.getElementById("gerar-tarefa");
+    console.log("tenho o Dom!")
+}
