2ea3e10dfc2505378d599a81864a9ac49b3488a0
Fayra Miranda <miranda.fayra@gmail.com>
2021-03-02 14:47:49 -0300

    Finalizar ex 2

--- SEMANA4/planner/planner/index.js
+++ SEMANA4/planner/planner/index.js
@@ -1,7 +1,14 @@
 // 1 - Crie uma função que é chamada ao clicar no botão "Criar Tarefa". 
 // Use um evento apropriado que identifique o clique no botão
 
+// 2 - Dentro da função, capture o valor do campo do HTML (que é a tarefa a ser colocada no planner)
+//  e armazene em uma variável
+
 function criarNovaTarefa(){
     const gerarTarefa = document.getElementById("gerar-tarefa");
-    console.log("tenho o Dom!")
+    // console.log("tenho o Dom!")
+    const minhaTarefa = document.getElementById("tarefa");
+    // console.log("Boaaa Garota!");
+
 }
+
