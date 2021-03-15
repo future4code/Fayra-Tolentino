d35ee87833ad1c9d81162d615b324222dfb796a9
Fayra Miranda <miranda.fayra@gmail.com>
2021-03-15 19:46:19 -0300

    Colocar forms

--- SEMANA6/formulario/src/App.js
+++ SEMANA6/formulario/src/App.js
@@ -1,49 +1,46 @@
 import React from 'react';
-import './App.css';
-import React, {useState} from 'react';
+// import IndexCss from './index.css';
 
-const [value, setValues] = useState({
-    fullName:'',
-    age:'',
-    email:'',
-    scholarity:''
-});
-
-<input
-    id="full-name"
-    class="form-field"
-    type="text"
-    placeholder="Digite seu nome completo"
-    name="fullName"
-    value={values.fullName}
-/>
-
-<input
-    id="age-person"
-    class="form-field"
-    type="text"
-    placeholder="Digite sua idade"
-    name="age"
-    value={values.age}
-/>
-
-<input
-    id="email"
-    class="form-field"
-    type="text"
-    placeholder="Digite seu e-mail"
-    name="email"
-    value={values.email}
-/>
-
-<input
-    id="scholarity-person"
-    class="form-field"
-    type="text"
-    placeholder="Digite sua escolaridade"
-    name="scholarity"
-    value={values.scholarity}
-/>
+export default function App (){
+    return (
+        <div class="form-container">
+          <form class="register-form">
+            <input
+              id="full-name"
+              class="form-field"
+              type="text"
+              placeholder="Digite seu nome"
+              name="fullname"
+            />
+            <input
+              id="age"
+              class="form-field"
+              type="text"
+              placeholder="Digite sua idade"
+              name="firstName"
+            />
+            
+            <input
+              id="schoolarship1"
+              class="form-field"
+              type="text"
+              placeholder="Digite sua escolaridade"
+              name="schoolarship"
+            />
+            <input
+              id="email"
+              class="form-field"
+              type="text"
+              placeholder="Email"
+              name="email"
+            />
+            <button class="form-field" type="submit">
+              Próxima Etapa
+            </button>
+          </form>
+        </div>
+      );
+    }
 
 
 
---
+++ SEMANA6/formulario/src/components/Parte1.js
@@ -0,0 +1,43 @@
+// export default function App (){
+//     return (
+//         <div class="form-container">
+//           <form class="register-form">
+//             <input
+//               id="full-name"
+//               class="form-field"
+//               type="text"
+//               placeholder="Digite seu nome"
+//               name="fullname"
+//             />
+//             <input
+//               id="age"
+//               class="form-field"
+//               type="text"
+//               placeholder="Digite sua idade"
+//               name="firstName"
+//             />
+            
+//             <input
+//               id="schoolarship1"
+//               class="form-field"
+//               type="text"
+//               placeholder="Digite sua escolaridade"
+//               name="schoolarship"
+//             />
+//             <input
+//               id="email"
+//               class="form-field"
+//               type="text"
+//               placeholder="Email"
+//               name="email"
+//             />
+//             <button class="form-field" type="submit">
+//               Próxima Etapa
+//             </button>
+//           </form>
+//         </div>
+//       );
+//     }
+
+
+
