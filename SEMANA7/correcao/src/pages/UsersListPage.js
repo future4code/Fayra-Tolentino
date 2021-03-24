fc50a14720017f87ed94f80225c1e9b606ddbd5d
Fayra Miranda <miranda.fayra@gmail.com>
2021-03-24 18:13:25 -0300

    Criar lista de usuário e login

---
+++ SEMANA7/correcao/src/pages/UsersListPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default class UserListPages extends React.Component{
+    render(){
+        return (
+            <div>
+                <h3>Listas de Usuários</h3>
+            </div>
+        )
+    }
+}
