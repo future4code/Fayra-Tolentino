c6b92415fb43fdfcf06e1c67f525b7d3aa0f1b60
Fayra Miranda <miranda.fayra@gmail.com>
2021-03-25 16:43:19 -0300

    Criar Lógica base da Api

--- SEMANA7/api_lyrics/src/App.js
+++ SEMANA7/api_lyrics/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react'
-// import Axios from 'axios'
+import axios from 'axios'
 // import Styled from 'styled-components' 
 
-export default class App extends React.Components{
+export default class App extends React.Component{
   state = {
     singer:'',
-    music:''
+    music:'',
+    lyrics:''
   }
 
   handleSinger = (event) => {
@@ -15,12 +16,38 @@ export default class App extends React.Components{
   handleMusic = (event) => {
     this.setState({music:event.target.value})
   }
-  
+
+  findLyrics = () =>{
+    axios.get(`https://api.lyrics.ovh/v1/${this.state.singer}/${this.state.music}`)
+    .then((response) => {
+      console.log(response);
+      this.setState({ lyrics:response.data.lyrics })
+
+    })
+    .catch((error) => {
+      this.setState({ lyrics:''})
+      console.log(error);
+    })
+  }
+
   render(){
-  <div>
-    <input></input>
-    <input></input>
-    <button></button>
-  </div>
+    console.log(this.state)
+    return(
+      <div>
+        <input
+          onChange={this.handleSinger}
+          value={this.state.singer}
+          placeholder="Artista"
+        />
+        <input
+          onChange={this.handleMusic}
+          value={this.state.music}
+          placeholder="Digite a Música"
+        />
+        <p>{this.state.lyrics}</p>
+        <button onClick={this.findLyrics}>Buscar Hit</button>
+      </div>
+    )
+  
   }
 }
