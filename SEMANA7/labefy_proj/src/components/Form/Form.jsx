94020865e150585892fe9ae580677ff038dcfc14
Fayra Miranda <miranda.fayra@gmail.com>
2021-03-26 16:10:06 -0300

    Estilizar Titulo, corrigir botão e input

--- SEMANA7/labefy_proj/src/components/Form/Form.jsx
+++ SEMANA7/labefy_proj/src/components/Form/Form.jsx
@@ -8,6 +8,13 @@ const FormBase = styled.div `
     flex-direction:column;
     padding-top:20vh;
 `
+const Title = styled.h1 `
+    font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', 
+    Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    font-weight:bolder;
+    font-size:60px;
+    color:#EE5A24;
+`
 
 const PlaylistCreate = styled.button `
     color:white;
@@ -18,7 +25,7 @@ const PlaylistCreate = styled.button `
     border-radius:160px;
     left:calc(50% - 75px);
     top:calc(50% - 25px);
-    background: linear-gradient(60deg, #f79533, #f37055, #ef4e7b, #a166ab, #5073b8, #1098ad, #07b39b, #6fba82);
+    background: linear-gradient(60deg, #EE5A24, #f37055);
     cursor:pointer;
     line-height:12px;
     
@@ -27,20 +34,20 @@ const MusicName = styled.input `
     width: 300px;
     height:42px;
     border:0 none;
-    border-bottom: 1px solid black;
+    border-bottom: 1px solid #EE5A24;
 `
 const InputContainer = styled.div `
     display:flex;
     justify-content:center;
     align-items:center;
-    padding-top: 20vh;
+    padding-top: 16vh;
 `
 
 export default class Form extends React.Component{
     render(){
         return(
             <FormBase>
-                <h1>Labefy</h1>
+                <Title>Labefy</Title>
                 <InputContainer>
                     <MusicName
                         // onChange={this.handleName}
