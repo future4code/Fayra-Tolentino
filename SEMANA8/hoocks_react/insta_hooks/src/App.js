7bcf5cd018f0610631d0e98ddd2ce2309b3f4f16
Fayra Miranda <miranda.fayra@gmail.com>
2021-04-05 15:29:08 -0300

    Criar array de post

---
+++ SEMANA8/hoocks_react/insta_hooks/src/App.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+const postArray = [
+  {
+    userName:"Fayra",
+    userPhoto: 'https://picsum.photos/50/50',
+    userPost: 'https://picsum.photos/200/150'
+  },
+  {
+    userName:"Jlo",
+    userPhoto: 'https://picsum.photos/50/51',
+    userPost: 'https://picsum.photos/200/151'
+  },
+  {
+    userName:"Shakira",
+    userPhoto: 'https://picsum.photos/50/52',
+    userPost: 'https://picsum.photos/200/152'
+  },
+  {
+    userName:"Veveta",
+    userPhoto: 'https://picsum.photos/50/53',
+    userPost: 'https://picsum.photos/200/153'
+  }
+]
+
+export default function Instafor(){
+  
+  
+  
+  return <div>
+    <p>Teste da Tela</p>
+  </div>
+}
+
