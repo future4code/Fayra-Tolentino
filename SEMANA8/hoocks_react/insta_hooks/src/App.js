ae5cdd60d810a7f9d94149493d8661fc61a62193
Fayra Miranda <miranda.fayra@gmail.com>
2021-04-05 16:46:42 -0300

    Criar array de post

--- SEMANA8/hoocks_react/insta_hooks/src/App.js
+++ SEMANA8/hoocks_react/insta_hooks/src/App.js
@@ -1,4 +1,7 @@
 import React from 'react'
+import styled from 'styled-components'
+
+// ____________________________Array de Posts________________
 
 const postArray = [
   {
@@ -22,6 +25,26 @@ const postArray = [
     userPost: 'https://picsum.photos/200/153'
   }
 ]
+
+// ____________________________Styled Components________________
+
+const AppContainer = styled.div `
+  display:flex;
+  justify-content:center;
+  flex-direction:column;
+  align-items:center;
+`
+const FormContainer = styled.div `
+  display:flex;
+  justify-content:space-space-between;
+  flex-direction:column;
+  height:180px;
+  width:240px;
+  border:1px solid gray;
+  margin:240px;
+  padding:30px;
+`
+
 
 export default function Instafor(){
   
