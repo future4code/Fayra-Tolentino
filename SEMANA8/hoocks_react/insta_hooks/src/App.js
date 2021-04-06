e8f41aaeca5fff1fd9c96a4eb73cbfff0d68cf3e
Fayra Miranda <miranda.fayra@gmail.com>
2021-04-06 15:05:01 -0300

    fazer exercicio hooks

--- SEMANA8/hoocks_react/insta_hooks/src/App.js
+++ SEMANA8/hoocks_react/insta_hooks/src/App.js
@@ -1,57 +1,33 @@
-import React from 'react'
-import styled from 'styled-components'
-
-// ____________________________Array de Posts________________
-
-const postArray = [
-  {
-    userName:"Fayra",
-    userPhoto: 'https://picsum.photos/50/50',
-    userPost: 'https://picsum.photos/200/150'
-  },
-  {
-    userName:"Jlo",
-    userPhoto: 'https://picsum.photos/50/51',
-    userPost: 'https://picsum.photos/200/151'
-  },
-  {
-    userName:"Shakira",
-    userPhoto: 'https://picsum.photos/50/52',
-    userPost: 'https://picsum.photos/200/152'
-  },
-  {
-    userName:"Veveta",
-    userPhoto: 'https://picsum.photos/50/53',
-    userPost: 'https://picsum.photos/200/153'
-  }
-]
-
-// ____________________________Styled Components________________
-
-const AppContainer = styled.div `
-  display:flex;
-  justify-content:center;
-  flex-direction:column;
-  align-items:center;
-`
-const FormContainer = styled.div `
-  display:flex;
-  justify-content:space-space-between;
-  flex-direction:column;
-  height:180px;
-  width:240px;
-  border:1px solid gray;
-  margin:240px;
-  padding:30px;
-`
+import React, { useState } from "react";
 
 
-export default function Instafor(){
-  
-  
-  
-  return <div>
-    <p>Teste da Tela</p>
-  </div>
+export default function App() {
+  const [textsList, setTextsList] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+
+  const handleInputValue = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const addToList = () => {
+    const newArray = [...textsList];
+    newArray.push(inputValue);
+    setTextsList(newArray);
+    setInputValue("");
+  };
+
+  const renderList = textsList.map((text) => {
+    return <p>{text}</p>;
+  });
+
+  return (
+    <div className="App">
+      <input onChange={handleInputValue} value={inputValue} />
+      <input onChange={handleInputValue} value={inputValue} />
+      <input onChange={handleInputValue} value={inputValue} />
+      <button onClick={addToList}>Adicionar</button>
+      {renderList}
+    </div>
+  );
 }
 
