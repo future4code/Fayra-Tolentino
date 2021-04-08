289d830f75576e7f57817f3c16287a2d81c02276
Fayra Miranda <miranda.fayra@gmail.com>
2021-04-08 13:22:57 -0300

    Adicionar botão do x

--- SEMANA8/tindao/src/components/Heart.js
+++ SEMANA8/tindao/src/components/Heart.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import IconButton from '@material-ui/core/IconButton';
 import FavoriteBorderRoundedIcon from '@material-ui/icons/FavoriteBorderRounded';
+import ClearRoundedIcon from '@material-ui/icons/ClearRounded';
+
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -15,9 +17,13 @@ export default function IconButtons() {
     const classes = useStyles();
     return (
     <div className={classes.root}>
-        <IconButton color="secondary" aria-label="add an alarm">
+        <IconButton size="large" color="secondary" aria-label="match">
+            <ClearRoundedIcon />
+        </IconButton>
+
+        <IconButton size="large" color="secondary" aria-label="match">
             <FavoriteBorderRoundedIcon />
-    </IconButton>
+        </IconButton>
     </div>
     );
 }
