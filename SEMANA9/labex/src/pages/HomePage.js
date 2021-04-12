56813af43f8d83eebefb398e63be717d4478c1ea
Fayra Miranda <miranda.fayra@gmail.com>
2021-04-12 16:02:04 -0300

    Criar Header, botões e estrutura principal

---
+++ SEMANA9/labex/src/pages/HomePage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from '../components/Header'
+
+
+function Home() {
+    return (
+        <div className="Home">
+            <Header/>
+            <button>Ver Viagens</button>
+            <button>Área Administrativa</button>
+        </div>
+    );
+}
+
+export default Home;
