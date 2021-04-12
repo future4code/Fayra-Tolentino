7aa9757c47ed96b6b05a4d2804671a7188271a66
Fayra Miranda <miranda.fayra@gmail.com>
2021-04-12 16:36:44 -0300

    Finalizar Base home

---
+++ SEMANA9/labex/src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import styled from "styled-components"
+
+
+// Cor header:#381463
+// Cor Footer:#652296
+const FooterDiv = styled.div `
+width:98.5vw;
+height:16vh;
+background-color:#381463;
+margin-top:20vh;
+`
+function Footer(){
+    return(
+        <FooterDiv/>  
+    )
+}
+export default Footer;
--- SEMANA9/labex/src/pages/HomePage.js
+++ SEMANA9/labex/src/pages/HomePage.js
@@ -1,13 +1,25 @@
 import React from 'react'
 import Header from '../components/Header'
+import Footer from '../components/Footer'
+import styled from 'styled-components'
 
+const ButtonContainer = styled.div `
+    display:flex;
+    justify-content:center;
+    align-items:center;
+    margin:240px 20px;
+    text-align:center;
+`
 
 function Home() {
     return (
         <div className="Home">
             <Header/>
-            <button>Ver Viagens</button>
-            <button>Área Administrativa</button>
+            <ButtonContainer>
+                <button>Ver Viagens</button>
+                <button>Área Administrativa</button>
+            </ButtonContainer>
+            <Footer/>
         </div>
     );
 }
