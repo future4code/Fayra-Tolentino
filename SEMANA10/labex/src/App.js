e621d04eb39d71cff87e0eec709ce1a741e4d8e5
Fayra Miranda <miranda.fayra@gmail.com>
2021-04-13 16:26:57 -0300

    Arrumar identação

--- SEMANA10/labex/src/App.js
+++ SEMANA10/labex/src/App.js
@@ -1,42 +1,11 @@
 import React from 'react'
-import { BrowserRouter, Switch, Route } from "react-router-dom";
-import Home from './pages/HomePage'
-import List from './pages/ListTripsPage'
-import Application from './pages/ApplicationFormPage'
-import Login from './pages/LoginPage'
-import AdminHome from './pages/AdminHomePage'
-import CreateTrip from './pages/CreateTripPage'
-import TripDetails from './pages/TripDetailsPage'
+import Router from './components/routes/Router';
 
 
 
 function App() {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/">
-          <Home/>
-        </Route>
-        <Route exact path="/trips/list">
-          <List/>
-        </Route>
-        <Route exact path="/trips/application">
-          <Application/>
-        </Route>
-        <Route exact path="/login">
-          <Login/>
-        </Route>
-        <Route exact path="/admin/trips/list">
-          <AdminHome/>
-        </Route>
-        <Route exact path="/admin/trips/create">
-          <CreateTrip/>
-        </Route>
-        <Route exact path="/admin/trips/id">
-          <TripDetails/>
-        </Route>
-      </Switch>
-    </BrowserRouter>
+    <Router/>
   );
 }
 
