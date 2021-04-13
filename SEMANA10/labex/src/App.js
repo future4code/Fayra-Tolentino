6f9836339672a0d8321901d9e56be37c2559f5cf
Fayra Miranda <miranda.fayra@gmail.com>
2021-04-13 15:34:06 -0300

    Importar páginas

--- SEMANA10/labex/src/App.js
+++ SEMANA10/labex/src/App.js
@@ -1,9 +1,23 @@
 import React from 'react'
+// import AdminHome from './pages/AdminHomePage'
+// import Application from './pages/ApplicationFormPage'
+// import CreateTrip from './pages/CreateTripPage'
+// import Home from './pages/HomePage'
+// import List from './pages/ListTripsPage'
+// import Login from './pages/LoginPage'
+import TripDetails from './pages/TripDetailsPage'
+
 
 function App() {
   return (
     <div className="App">
-
+      {/* <AdminHome/> */}
+      {/* <Application/> */}
+      {/* <CreateTrip/> */}
+      {/* <Home/> */}
+      {/* <List/> */}
+      {/* <Login/> */}
+      <TripDetails/>
     </div>
   );
 }
