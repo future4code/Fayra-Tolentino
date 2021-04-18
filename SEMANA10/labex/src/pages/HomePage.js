06e2835c97823a6f3fc68d89ce1199ec64dd93b8
Fayra Miranda <miranda.fayra@gmail.com>
2021-04-18 12:59:16 -0300

    Finalizar base de estilização

--- SEMANA10/labex/src/pages/HomePage.js
+++ SEMANA10/labex/src/pages/HomePage.js
@@ -11,13 +11,13 @@ const HomePage = styled.div `
     justify-content:center;
     align-items:center;
     flex-direction:column;
-    padding-top:32vh;
+    padding-top:28vh;
+    font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 
+    'Helvetica Neue', sans-serif;
 `
-
 const ButtonsContainer = styled.div `
     padding-top:4vh;
 `
-
 const ButtonStyled = styled.button `
     height:40px;
     width:200px;
@@ -46,15 +46,24 @@ const ButtonStyled = styled.button `
         left: 100%;
     }
 `
-
-
+const Title = styled.h1 `
+    color:#250340;
+    font-size:3.2rem;
+`
+const SubTitle = styled.h1 `
+    color:#250340;
+    font-size:1rem;
+    font-weight:500;
+    margin-bottom:20px;
+`
+// ----------------------------------------------------------------------------------------------------------------------------------------
 
 function Home() {
     const history = useHistory();
     return (
         <HomePage>
-            <h1>LabeX</h1>
-            <p>Encontre as melhores viagens espaciais!</p>
+            <Title>LabeX</Title>
+            <SubTitle>Encontre as melhores viagens espaciais!</SubTitle>
             <ButtonsContainer>
                 <ButtonStyled onClick={() => goToList(history)}>Ver Viagens</ButtonStyled>
                 <ButtonStyled onClick={() => goToLogin(history)}> Administrador</ButtonStyled>
