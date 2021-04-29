4778d75e34b32845d2b0ae539613e8db470514b4
Fayra Miranda <miranda.fayra@gmail.com>
2021-04-29 14:47:47 -0300

    Renderizar cards de noticia na tela

--- SEMANA12-REV/labeddit/src/pages/FeedPage/FeedPage.js
+++ SEMANA12-REV/labeddit/src/pages/FeedPage/FeedPage.js
@@ -1,12 +1,30 @@
 import React from 'react'
 import useProtectedPage from '../../hooks/useProtectedPage'
 import NewsCard from '../../components/NewsCard/NewsCard'
+import useRequestData from '../../hooks/useRequestData'
+import BASE_URL from '../../constants/urls'
 
 export const Feed =()=>{
     useProtectedPage()
+    const newsPost = useRequestData([],`${BASE_URL}/posts`)
+    console.log(newsPost)
+
+    const newsCards = newsPost.posts && newsPost.posts.map((postCard)=>{
+        return(
+            <NewsCard
+                key ={postCard.id}
+                title = {postCard.title}
+                userName={postCard.username}
+                text={postCard.text}
+                date ={postCard.createdAt}
+            /> 
+        )
+
+    })
+
     return(
         <div>
-            <NewsCard/>
+            {newsCards}
         </div>
     )
 }
