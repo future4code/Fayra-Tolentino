4018d07e8e3bd661896e9ff89f49f5be1a3cb48f
Fayra Miranda <miranda.fayra@gmail.com>
2021-05-01 19:04:37 -0300

    Adicionar rotas

--- SEMANA12-REV/labeditt/src/routes/routes.js
+++ SEMANA12-REV/labeditt/src/routes/routes.js
@@ -1,3 +1,45 @@
 import React from 'react'
 import {BrowserRouter,Switch,Route} from 'react-router-dom'
+import LoginPage from '../pages/LoginPage/LoginPage'
+import FeedPage from '../pages/FeedPage/FeedPage'
+import CreatePostPage from '../pages/CreatePostPage/CreatePostPage'
+import PostDetailPage from '../pages/PostDetailPage/PostDetailPage'
+import SignUpPage from '../pages/SignUpPage/SignUpPage'
+import ErrorPage from '../pages/ErrorPage/ErrorPage'
+
+
+export const Router = () =>{
+    return(
+        <BrowserRouter>
+            <Switch>
+                <Route exact path = '/'>
+                    <LoginPage/>
+                </Route>
+
+                <Route exact path = '/feed'>
+                    <FeedPage/>
+                </Route>
+
+                <Route exact path = '/create-post'>
+                    <CreatePostPage/>
+                </Route>
+
+                <Route exact path = '/post-detail'>
+                    <PostDetailPage/>
+                </Route>
+
+                <Route exact path = 'signUp'>
+                    <SignUpPage />
+                </Route>
+
+                <Route>
+                    <ErrorPage/>
+                </Route>
+
+            </Switch>
+        </BrowserRouter>
+    )
+}
+
+export default Router;
 
