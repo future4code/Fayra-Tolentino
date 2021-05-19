d34b06feaa99340cf8cd6ab0240829e9eefc5e8a
Fayra Miranda <miranda.fayra@gmail.com>
2021-05-19 16:40:29 -0300

    Finalizar exs 1 e 2

--- SEMANA15/D3/src/index.ts
+++ SEMANA15/D3/src/index.ts
@@ -22,26 +22,54 @@
 //             name: country.name
 //         }))
         
+// ******************************************************* ENDPOINT 1 ************************************************************
 
+// import express, { Request, Response } from 'express'
+// import cors from 'cors'
+// import { countries } from './countries'
+// import { AddressInfo } from "net";
+// const app = express()
+
+//     app.use(express.json())
+//     app.use(cors())
+
+//     app.get('/countries/all' , (req: Request, res: Response ) => {
+//         const result = countries.map(country => ({
+//             id: country.id,
+//             name: country.name
+//         }))
+        
+//         res.
+//         status(200).
+//         send(result)
+//     })
+
+//     const server = app.listen(process.env.PORT || 3003, () => {
+//         if (server) {
+//         const address = server.address() as AddressInfo ;
+//         console.log(`Server is running in http://localhost: ${address.port}`);
+//         } else {
+//         console.error(`Failure upon starting server.` );
+//         }
+//     });
+
+// ******************************************************* ENDPOINT 2 ************************************************************
 
 import express, { Request, Response } from 'express'
 import cors from 'cors'
 import { countries } from './countries'
 import { AddressInfo } from "net";
+
 const app = express()
 
     app.use(express.json())
     app.use(cors())
 
-    app.get('/countries/all' , (req: Request, res: Response ) => {
-        const result = countries.map(country => ({
-            id: country.id,
-            name: country.name
-        }))
-        
+    app.get('/countries/:id' , (req: Request, res: Response ) => {
+        const id = req.params.id
         res.
         status(200).
-        send(result)
+        send(countries.find((countries=> countries.id)))
     })
 
     const server = app.listen(process.env.PORT || 3003, () => {
