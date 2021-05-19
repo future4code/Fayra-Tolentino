215cfdf5ce1766467a70ed9c92af7b3802c54d22
Fayra Miranda <miranda.fayra@gmail.com>
2021-05-19 19:42:33 -0300

    Finalizar exercicio 3

--- SEMANA15/D3/src/index.ts
+++ SEMANA15/D3/src/index.ts
@@ -55,28 +55,149 @@
 
 // ******************************************************* ENDPOINT 2 ************************************************************
 
-import express, { Request, Response } from 'express'
+// import express, { Request, Response } from 'express'
+// import cors from 'cors'
+// import { countries } from './countries'
+// import { AddressInfo } from "net";
+
+// const app = express()
+
+//     app.use(express.json())
+//     app.use(cors())
+
+//     app.get('/countries/:id' , (req: Request, res: Response ) => {
+//         const id = req.params.id
+//         res.
+//         status(200).
+//         send(countries.find((countries=> countries.id)))
+//     })
+
+//     const server = app.listen(process.env.PORT || 3003, () => {
+//         if (server) {
+//         const address = server.address() as AddressInfo ;
+//         console.log(`Server is running in http://localhost: ${address.port}`);
+//         } else {
+//         console.error(`Failure upon starting server.` );
+//         }
+//     });
+
+// ******************************************************* ENDPOINT 3  TENTATIVA 1************************************************************
+// import express,{Request,Response} from 'express'
+// import cors from 'cors'
+// import {countries} from './countries'
+// import {AddressInfo} from 'net'
+
+// const app = express()
+// app.use(express.json())
+// app.use(cors())
+
+// app.get('/countries/search',(req:Request, res:Response)=>{
+    // const name = req.params.name as string
+    // const capital = req.params.capital as string
+    // const continent = req.params.continent as string
+    // const result = countries.filter((country)=> country.name.toLocaleLowerCase().includes(name.toLocaleLowerCase()))
+    
+    // 1. pq isso dá ruim?
+    // .send(countries.find((countries=> countries.name )|| (countries=> countries.capital)||(countries=> countries.continent)))
+
+    // if(result.length){
+    //     res
+    //     .status(200)
+    //     .send(result)
+    // }else{
+    //     res
+    //     .status(400)
+    //     .send('Not found')
+    // }
+
+    // 2. pq isso deu ruim 2?
+    // if(name){
+    //     result = countries.filter((country)=> country.name.toLocaleLowerCase().includes(name.toLocaleLowerCase()))
+    // }else{
+    //     res
+    //     .status(400)
+    //     .send('Not found')
+    // }
+
+// })
+
+
+// app.get('/countries/search',(req:Request, res:Response)=>{
+//     try{
+//         if(req.params.name !== string){
+//             throw new Error('Tem que ser uma string')
+//         }
+//         const name = req.params.name as string
+//         res
+//         .status(400)
+//         .send((countries.find(country=>country.name === name)))
+//     }catch(error){
+//         console.log('Deu ruim')
+//     }
+// })
+
+
+// app.get('/countries/search',(req:Request, res:Response)=>{}
+
+
+// const server = app.listen(process.env.PORT||3003,()=>{
+//     if(server){
+//         const address = server.address()as AddressInfo;
+//         console.log(`Server is running in https://localhost:${address.port}`)
+//     }else{
+//         console.log(`Failure upon starting server.`)
+//     }
+// })
+
+// ******************************************************* ENDPOINT 3 ************************************************************
+import express,{Request,Response} from 'express'
 import cors from 'cors'
-import { countries } from './countries'
-import { AddressInfo } from "net";
+import {countries} from './countries'
+import {AddressInfo} from 'net'
 
 const app = express()
+app.use(express.json())
+app.use(cors())
 
-    app.use(express.json())
-    app.use(cors())
-
-    app.get('/countries/:id' , (req: Request, res: Response ) => {
-        const id = req.params.id
-        res.
-        status(200).
-        send(countries.find((countries=> countries.id)))
-    })
-
-    const server = app.listen(process.env.PORT || 3003, () => {
-        if (server) {
-        const address = server.address() as AddressInfo ;
-        console.log(`Server is running in http://localhost: ${address.port}`);
-        } else {
-        console.error(`Failure upon starting server.` );
+app.get('/countries/search',(req:Request, res:Response)=>{
+    try{
+        let result: country[] = countries
+        if (req.query.name) {
+            result = result.filter(
+                country => country.name.includes(req.query.name as string)
+            )
         }
-    });
+        if (req.query.capital) {
+            result = result.filter(
+            country => country.capital.includes(req.query.capital as string)
+            )
+        }
+        if (req.query.continent) {
+            result = result.filter(
+            country => country.continent.includes(req.query.continent as string)
+            )
+        }
+
+        if(!result){
+            throw new Error ('Nenhum resultado obtido')
+        }
+
+        res
+        .status(200)
+        .send(result)
+    }
+    catch(error){
+        res
+        .status(400)
+        .send({message:error.message})
+    }
+})
+
+const server = app.listen(process.env.PORT||3003,()=>{
+        if(server){
+            const address = server.address()as AddressInfo;
+            console.log(`Server is running in https://localhost:${address.port}`)
+        }else{
+            console.log(`Failure upon starting server.`)
+        }
+    })
