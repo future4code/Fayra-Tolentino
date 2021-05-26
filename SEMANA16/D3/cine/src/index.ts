363d6da0217c706610ee9e8e2cbf9e3c81b267d1
Fayra Miranda <miranda.fayra@gmail.com>
2021-05-26 16:37:35 -0300

    Finalizar item 1b

--- SEMANA16/D3/cine/src/index.ts
+++ SEMANA16/D3/cine/src/index.ts
@@ -1,11 +1,55 @@
-import express from 'express';
+import express, { Request, Response } from "express";
+import knex from 'knex';
 import cors from 'cors';
 import { AddressInfo } from 'net';
+import connection from './connection';
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+const getActorByName =async(name:string):Promise<any> =>{
+    const result =await connection.raw(`
+    SELECT * FROM Actor_Table WHERE name = '${name}'
+    `)
+    console.log(result)
+    console.log
+    return result [0] [0]
+}
+app.get("/users/:name",async(req:Request, res:Response)=>{
+    try{
+        const name = req.params.name
+        console.log(name)
+        console.log(await getActorByName(name))
+        res.end()
+    }catch (error){
+        console.log(error.message)
+        res.status(500).send("Unexpected error")
+    }
+})
+
+const getActorById =async(id:string):Promise<any> =>{
+    const result =await connection.raw(`
+    SELECT * FROM Actor_Table WHERE id = '${id}'
+    `)
+    return result [0] [0]
+}
+
+app.get("/users/:id", async (req: Request, res: Response) => {
+    try {
+        const id = req.params.id
+        // let actorResponse = console.log(await getActorById(id))
+        // res.send(`Your actor is ${actorResponse}`) -> Pq deu undefined
+        console.log(await getActorById(id))
+        res.end()
+    } catch (error) {
+        console.log(error.message)
+        res.status(500).send("Unexpected error")
+    }
+})
+
+
 
 const server = app.listen(process.env.PORT||3003,()=>{
     if(server){
