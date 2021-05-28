10b9eabd498463eff9070ef63922f8784cb5e23d
Fayra Miranda <miranda.fayra@gmail.com>
2021-05-28 14:34:21 -0300

    Fazer criação de usuário

--- SEMANA16/PROJETO/TO-DO/src/index.ts
+++ SEMANA16/PROJETO/TO-DO/src/index.ts
@@ -1,11 +1,54 @@
-import express,{Express} from 'express'
+import express,{Request,Response} from 'express'
 import cors from 'cors'
 import {AddressInfo} from 'net'
 
-const app: Express = express()
+const app = express()
+
+type User ={
+    name:string,
+    nickname:string,
+    email:string
+}
+
+let users: User[]=[
+    {
+        name:'Angela Montenegro',
+        nickname:'Montenegro',
+        email:'montenegro@jeffersonian.com'
+    },
+    {
+        name:'Jack Hodgins',
+        nickname:'Hodgins',
+        email:'hodgins@jeffersonian.com'
+    }
+]
+
 
 app.use(express.json())
 app.use(cors())
+
+app.post('/user',(req:Request,res:Response)=>{
+    const reqBody = req.body
+    try{
+        
+        if(!req.body.name || !req.body.nickname || !req.body.email ){
+            throw new Error ('Please, check your information! You need complete all! of then!')
+        }
+        const newUser: User = {
+            name : reqBody.name,
+            nickname : reqBody.nickname,
+            email : reqBody.email
+        }
+        
+        users.push(newUser)
+        res.status(200).send(newUser)
+        console.log(newUser)
+
+    }catch(error){
+        res.status(400).send({message:error.message})
+        console.log('Deu ruim')
+    }
+})
 
 const server = app.listen(process.env.PORT || 3003, () => {
     if (server) {
