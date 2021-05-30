6ed7c24bd2943f5d53235b499d729ab86e8cb7a9
Fayra Miranda <miranda.fayra@gmail.com>
2021-05-30 18:01:27 -0300

    Finalizar item 4

--- SEMANA16/PROJETO/TO-DO/src/index.ts
+++ SEMANA16/PROJETO/TO-DO/src/index.ts
@@ -11,6 +11,13 @@ type User ={
     email:string
 }
 
+type Task ={
+    title : string,
+	description : string,
+	limitDate : string,
+    creatorUserId:number
+}
+
 
 app.use(express.json())
 app.use(cors())
@@ -33,6 +40,25 @@ app.post('/user',async(req:Request,res:Response)=>{
         await connection('TodoListUser').insert(newUser)
         res.status(200).send(`User ${newUser.name} created successfully! `)
 
+    }catch(error){
+        res.status(400).send({message:error.message})
+        console.log({message:error.message})
+    }
+})
+
+//Create Task
+
+app.post('/task',async(req:Request,res:Response)=>{
+    const reqBody = req.body
+    try{
+        const newTask: Task ={
+            title : reqBody.title,
+            description : reqBody.description,
+            limitDate : reqBody.limitDate,
+            creatorUserId:reqBody.creatorUserId
+        }
+        await connection('TaskTable').insert(newTask)
+        res.status(200).send(`Task ${newTask.title} created successfully! `)
     }catch(error){
         res.status(400).send({message:error.message})
         console.log({message:error.message})
@@ -86,6 +112,8 @@ app.get('/user/:id',async(req:Request,res:Response)=>{
     }
 })
 
+
+//Server
 
 const server = app.listen(process.env.PORT || 3003, () => {
     if (server) {
