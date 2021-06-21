563a55b8bcec43eb1962f7c17650a67f61d1e862
Fayra Miranda <miranda.fayra@gmail.com>
2021-06-21 15:54:08 -0300

    Finalizar ex2 b

--- SEMANA20/D1/tests/ex2.test.ts
+++ SEMANA20/D1/tests/ex2.test.ts
@@ -4,24 +4,35 @@ import performPurchase from '../src/ex1'
 
 // a. Faça um teste com um usuário com o saldo maior do que o valor de compra
 
-describe('Testing if balance is greater than purchase',()=>{
-    test('should return user if the balance is greater than the purchase amount',()=>{
+// describe('Testing if balance is greater than purchase',()=>{
+//     test('should return user if the balance is greater than the purchase amount',()=>{
+//         const user: User = {
+//             name: "Fayra",
+//             balance: 1000
+//         }
+//         const output = performPurchase(user ,200)
+//         expect(output).toEqual({
+//             name: "Fayra",
+//             balance: 800
+//         })
+//     })
+// })
+
+
+// b. Faça um teste com um usuário com o saldo igual ao valor de compra
+
+describe('Testing if balance is equal purchase',()=>{
+    test('should return user if the balance is is equal purchase',()=>{
         const user: User = {
             name: "Fayra",
             balance: 1000
         }
-        const output = performPurchase(user ,200)
+        const output = performPurchase(user,1000)
         expect(output).toEqual({
             name: "Fayra",
-            balance: 800
+            balance: 0
         })
     })
 })
-
-
-	
-	
-
-// b. Faça um teste com um usuário com o saldo igual ao valor de compra
 
 // c. Faça um teste com um usuário com o saldo menor do que o valor de compra
