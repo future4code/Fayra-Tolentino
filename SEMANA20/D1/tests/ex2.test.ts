66dc796ebec36a31018538ec2db232344dbbc906
Fayra Miranda <miranda.fayra@gmail.com>
2021-06-21 16:11:10 -0300

    Finalizar ex 2 c

--- SEMANA20/D1/tests/ex2.test.ts
+++ SEMANA20/D1/tests/ex2.test.ts
@@ -10,7 +10,9 @@ import performPurchase from '../src/ex1'
 //             name: "Fayra",
 //             balance: 1000
 //         }
+
 //         const output = performPurchase(user ,200)
+
 //         expect(output).toEqual({
 //             name: "Fayra",
 //             balance: 800
@@ -21,18 +23,35 @@ import performPurchase from '../src/ex1'
 
 // b. Faça um teste com um usuário com o saldo igual ao valor de compra
 
-describe('Testing if balance is equal purchase',()=>{
-    test('should return user if the balance is is equal purchase',()=>{
-        const user: User = {
-            name: "Fayra",
-            balance: 1000
-        }
-        const output = performPurchase(user,1000)
-        expect(output).toEqual({
-            name: "Fayra",
-            balance: 0
-        })
-    })
-})
+// describe('Testing if balance is equal purchase',()=>{
+//     test('should return user if the balance is equal purchase',()=>{
+//         const user: User = {
+//             name: "Fayra",
+//             balance: 1000
+//         }
+
+//         const output = performPurchase(user,1000)
+
+//         expect(output).toEqual({
+//             name: "Fayra",
+//             balance: 0
+//         })
+//     })
+// })
 
 // c. Faça um teste com um usuário com o saldo menor do que o valor de compra
+
+describe('Testing if balance lower than purchase',()=>{
+    test('should return undefined if the balance is lower than purchase',()=>{
+        const user: User = {
+            name: "Fayra",
+            balance: 50
+        }
+
+        const output = performPurchase(user,1000)
+
+        expect(output).toEqual(
+            undefined
+        )
+    })
+})
