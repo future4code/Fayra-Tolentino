54985ee1f182c3d1e8f2c428ccc17bac210a4edb
Fayra Miranda <miranda.fayra@gmail.com>
2021-06-21 15:06:54 -0300

    Criar base teste

---
+++ SEMANA20/D1/src/tests/ex2.test.ts
@@ -0,0 +1,5 @@
+// a. Faça um teste com um usuário com o saldo maior do que o valor de compra
+
+// b. Faça um teste com um usuário com o saldo igual ao valor de compra
+
+// c. Faça um teste com um usuário com o saldo menor do que o valor de compra
